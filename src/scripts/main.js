8285426385911e1cb1d6a4eb6e0b8663a83d2060
Alex Sinchuk <120649140+alexsinchuk@users.noreply.github.com>
2026-08-28 11:35:30 +0200

    solution one form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const forms = [...document.querySelectorAll('form')];
-const inputs = [...forms.querySelectorAll('input')];
 
-inputs.forEach((input) => {
+const form = document.querySelector('form');
+const inputs = [...form.querySelectorAll('input')];
+
+inputs.forEach(input => {
   const label = document.createElement('label');
-  const fieldName = input.name[0].toUpperCase() + input.name.slice(1);
+
+  const fieldName =
+    input.name[0].toUpperCase() + input.name.slice(1);
 
   label.classList.add('field-label');
-
   label.setAttribute('for', input.id);
-
   label.textContent = fieldName;
 
   input.placeholder = fieldName;
 
   input.parentElement.appendChild(label);
 });
+
